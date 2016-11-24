bf4db89b5e9089da0a83dc7ca86d86945af2a1b1
Connor Hannough <12ch33@queensu.ca>
2016-11-24 10:38:57 -0800

    Mapping question 3

---
+++ capitalizer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#Assignment_4 Question_3 - Capitalizer.c program
+#Capitalize the first letter of every other word
+#Start from first word of each line
+
+#Program updates the file with capitals
+
+#Take the filename to capitalize its text as a - commant-line argument - for testing
+#use poem.txt 
+
+
+int main() {
+
+printf("\nhello\n\n");
+
+return 0;
+
+}
+
