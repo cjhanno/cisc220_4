46be45b80304aa4c7de95e52ad5b71fc5fcb1238
Connor Hannough <12ch33@queensu.ca>
2016-11-24 13:50:38 -0800

    capitalizer file edits

--- capitalizer.c
+++ capitalizer.c
@@ -1,28 +1,53 @@
 #include<stdio.h>
 
-#include "poem.txt"
-
-
-
-val=sizeof(poem.txt); 
-
 //#Assignment_4 Question_3 - Capitalizer.c program
 //#Capitalize the first letter of every other word
 //#Start from first word of each line
-
 //#Program updates the file with capitals
-
 //#Take the filename to capitalize its text as a - commant-line argument - for testing
 //#use poem.txt 
 
+//FILE *poems;
+//poems = fopen("poem.txt","r+");
+//char poetry[333];
+//fscanf(poems, "%s", poetry);
+
+
+//int a;
+//for( a=0; a<333; a=a+1 ) {
+//      printf("value of a: %c\n", poetry[a]);
+//}
+//while(1) {
+//      if( feof(poems) ) {
+//              break;}
+//      printf("%s",poetry);
+//while(1) {
+//        fgets(buff, 255, fp);
+//       if( feof(fp) ) {
+//                break ;}
+//        printf("%s", buff);
+//}
 
 int main() {
 
-printf("\nhello\n\n");
-printf(val);
-return 0;
+FILE* fp;
+char buff[255];
 
+fp = fopen("poem.txt","w");
+fprintf(fp, "this is testing...\n line2\n");
+fclose(fp); 
+
+fp = fopen("poem.txt", "r");
+fscanf(fp, "%s", buff);
+printf("1: %s\n", buff);
+
+fgets(buff, 255, fp);
+printf("2: %s\n", buff );
+
+fgets(buff, 255, fp);
+printf("3: %s\n", buff);
+fclose(fp);
+
+return 0;
 }
 
-
-:wq`
