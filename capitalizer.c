47eeea61f6dc28ffe10c3509e35d44cd6d4e8f4a
Connor Hannough <12ch33@queensu.ca>
2016-11-24 12:11:21 -0800

    i dont know whats happening

--- capitalizer.c
+++ capitalizer.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
 
-#Assignment_4 Question_3 - Capitalizer.c program
-#Capitalize the first letter of every other word
-#Start from first word of each line
+#include "poem.txt"
 
-#Program updates the file with capitals
 
-#Take the filename to capitalize its text as a - commant-line argument - for testing
-#use poem.txt 
+
+val=sizeof(poem.txt); 
+
+//#Assignment_4 Question_3 - Capitalizer.c program
+//#Capitalize the first letter of every other word
+//#Start from first word of each line
+
+//#Program updates the file with capitals
+
+//#Take the filename to capitalize its text as a - commant-line argument - for testing
+//#use poem.txt 
 
 
 int main() {
 
 printf("\nhello\n\n");
-
+printf(val);
 return 0;
 
 }
 
+
+:wq`
